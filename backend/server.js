6ab0234c950ef87cbf48d01be41dce175f24f7b8
pseudo0244 <142434147+pseudo0244@users.noreply.github.com>
2025-09-27 19:19:47 +0530

    Dashboard added with api fetch

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import healthRoutes from './routes/healthRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(helmet());
